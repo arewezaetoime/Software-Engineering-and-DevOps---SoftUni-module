97dce07391e676d08159429c0694959ff8a2d086
hristoiliev <hristoiliev.1994@gmail.com>
2024-06-03 21:27:37 +0300

    complete the 4 testts

--- 04_Automated_Testing/to-do-app/tests/todo.test.js
+++ 04_Automated_Testing/to-do-app/tests/todo.test.js
@@ -21,3 +21,25 @@ test('User delete task', async ({ page }) => {
         tasks => tasks.map(task => task.textContent));
     expect(tas).not.toContain('delete task');
 })
+
+test('User can complete task', async ({ page }) => {
+    await page.goto('http://127.0.0.1:5500/04_Automated_Testing/to-do-app/index.html')
+    await page.fill('#task-input', 'complete task')
+    await page.click('#add-task')
+    
+    // Delete the task deiba
+    await page.click('.task .complete-task');
+
+    const completedTasks = await page.$('.task.completed');
+    expect(completedTasks).not.toBeNull();
+})
+
+test('USer can filter tasks', async ({ page }) => {
+    await page.goto('http://127.0.0.1:5500/04_Automated_Testing/to-do-app/index.html')
+    await page.fill('#task-input', 'filter task')
+    await page.click('#add-task')
+
+    await page.click('.task .task-complete');
+    const incompleteTask = await page.$('.task:not(.completed)')
+    expect(incompleteTask).toBeNull();
+})
