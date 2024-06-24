8e57a2fa0573313d22d5b2c04a520d8fa90264f4
hristoiliev <hristoiliev.1994@gmail.com>
2024-06-24 16:12:59 +0300

    asa

--- 03_Version_Control_with_Git/tete.py
+++ 03_Version_Control_with_Git/tete.py
@@ -1,10 +1,14 @@
-import math
+def unique(n):
+    copy_arr = n.copy()
 
+    for i in range(0, len(n) + 1):
+        if i + 1 == len(n):
+            break
 
-fucking_arr = [1,2,3,4, '3']
+        if n[i] != n[i + 1]:
+            continue
+        else:
+            copy_arr.pop(n[i])
 
-print(fucking_arr.__len__)
-print(len(fucking_arr))
-
-arr = list(map(int, fucking_arr))
-print(arr)
+        return copy_arr
+    
